47456075476d6453847df64e30ebbe2909f2b938
Kylin93CN <375020612@qq.com>
2019-03-26 18:16:56 +0800

    code commit for rep

---
+++ nuxt.issue.js
@@ -0,0 +1,23 @@
+// nuxt.config.js
+head: {
+  title: '杨超越公益官网',
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: pkg.description },
+    { name: 'referrer', content: 'no-referrer' }
+  ],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+},
+
+
+// static file
+favicon.ico
+
+// after build
+// .nuxt/dist/client   can't find  favicon.ico
+102029e724febf00f0dd.js  8c85defb1521493b27d1.js  bd6af4fe4f83911791f0.js  img
+4f6cc5460e4a856d6ce9.js  9d430fef3bafade1ff0e.js  ccfa2c0d148f261da1a3.js  LICENSES
+5c228c9193f158b483e4.js  ab5a7b3d2c69617cc913.js  fonts
+
+
